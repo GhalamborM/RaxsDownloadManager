9648ca5588fc2977e47d913996718c76b0bdd63d
ramtin <ramtinak@live.com>
2021-01-12 05:49:15 +0330

    Add UserAgentHelper

---
+++ src/WDM.Helpers/UserAgentHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WDM.Helpers
+{
+    static class UserAgentHelper
+    {
+        public const string GOOGLE_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36";
+        public const string ANDROID_GOOGLE_USER_AGENT = "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
+
+    }
+}
