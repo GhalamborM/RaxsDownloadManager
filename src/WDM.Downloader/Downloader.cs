2f98368434c3e633ede615c74e3b1093ba794fff
ramtin <ramtinak@live.com>
2021-01-17 01:30:25 +0330

    Add NotSupportedSchemeException

--- src/WDM.Downloader/Downloader.cs
+++ src/WDM.Downloader/Downloader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using WDM.Downloaders.Exceptions;
 using WDM.Downloaders.Storage;
 
 namespace WDM.Downloaders
@@ -11,5 +13,11 @@ namespace WDM.Downloaders
         public DownloadInfo DownloadInfo { get; internal set; }
         public FileStorage FileStorage { get; internal set; }
 
+        public async Task ConfigureAsync(Uri uri)
+        {
+            if(!uri.Scheme.ToLower().StartsWith("http"))
+                throw new NotSupportedSchemeException($"'{uri.Scheme}' scheme is not supported");
+
+        }
     }
 }
---
+++ src/WDM.Downloader/Exceptions/NotSupportedSchemeException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WDM.Downloaders.Exceptions
+{
+    public class NotSupportedSchemeException : Exception
+    {
+        public NotSupportedSchemeException(string message) : base(message) { }
+    }
+}
