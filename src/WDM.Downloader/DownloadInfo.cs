f49e3b80436e74a067116b67e8a74c891c0ddd5e
ramtin <ramtinak@live.com>
2021-01-16 23:00:16 +0330

    Add DownloadInfo

---
+++ src/WDM.Downloader/DownloadInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+
+namespace WDM.Downloader
+{
+    public struct DownloadInfo
+    {
+        public string ServerSideFileName { get; internal set; }
+        public string ContentType { get; internal set; }
+        public long? ContentLength { get; internal set; }
+        public bool PartialContent { get; internal set; }
+        public bool CanDetectPartialContent { get; internal set; }
+        public HttpStatusCode StatusCode { get; internal set; }
+    }
+}
