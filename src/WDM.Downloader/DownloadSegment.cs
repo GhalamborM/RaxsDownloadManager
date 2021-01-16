c47c0680bcd021f54fc843b6e6c1d6522e54fc64
ramtin <ramtinak@live.com>
2021-01-17 00:41:32 +0330

    Add IsDownloading to DownloadSegment

--- src/WDM.Downloader/DownloadSegment.cs
+++ src/WDM.Downloader/DownloadSegment.cs
@@ -7,6 +7,7 @@ namespace WDM.Downloader
     public class DownloadSegment
     {
         public string Path { get; internal set; }
+        public bool IsDownloading { get; internal set; }
         public long? StartPosition { get; internal set; }
         public long? EndPosition { get; internal set; }
         public DownloadSegmentState State { get; internal set; }
