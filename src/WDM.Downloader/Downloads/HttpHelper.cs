c9c5616df39ad8ba9f3accc7ccc0b85876b1bcea
ramtin <ramtinak@live.com>
2021-01-20 23:23:28 +0330

    Add GetClientHandler to HttpHelper

--- src/WDM.Downloader/Downloads/HttpHelper.cs
+++ src/WDM.Downloader/Downloads/HttpHelper.cs
@@ -12,5 +12,8 @@ namespace WDM.Downloaders.Downloads
         static public readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(30);
 
         static public HttpClient GetClient(TimeSpan timeout = default) => new HttpClient { Timeout = timeout == default ? DefaultTimeout : timeout };
+
+        static public HttpClientHandler GetClientHandler(IWebProxy proxy = null) =>
+            new HttpClientHandler { Proxy = proxy, UseProxy = proxy != null };
     }
 }
