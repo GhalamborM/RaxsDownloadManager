a759db499c498349b370f0e19b4d55f023adaeda
ramtin <ramtinak@live.com>
2021-01-20 23:20:58 +0330

    Add HttpHelper Add GetClient to HttpHelper

---
+++ src/WDM.Downloader/Downloads/HttpHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDM.Downloaders.Downloads
+{
+    static class HttpHelper
+    {
+        static public readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(30);
+
+        static public HttpClient GetClient(TimeSpan timeout = default) => new HttpClient { Timeout = timeout == default ? DefaultTimeout : timeout };
+    }
+}
