d7b414a763f6add60c1b66c7fb42193849cbb3c1
ramtin <ramtinak@live.com>
2021-01-21 00:34:00 +0330

    Update GetInfoAsync Bugfix for files larger than 2GB

--- src/WDM.Downloader/Downloads/DownloadChecker.cs
+++ src/WDM.Downloader/Downloads/DownloadChecker.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using static WDM.Downloaders.Downloads.HttpHelper;
 
 namespace WDM.Downloaders.Downloads
 {
@@ -22,44 +23,39 @@ namespace WDM.Downloaders.Downloads
         /// <param name="uri">Uri</param>
         public static async Task<DownloadInfo> GetInfoAsync(Uri uri)
         {
-            using (var client = new HttpClient { Timeout = TimeSpan.FromSeconds(40) })
-            using (var request = new HttpRequestMessage(HttpMethod.Head, uri))
-            using (var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead))
-            // Use ResponseHeadersRead to prevent a exception related to files larger than 2GB.
+            using (var client = GetClient())
+            using (var request = GetRequest(uri, null, true))
+            using (var response = await client.SendAsync(request))
+            // Use ResponseHeadersRead to prevent a exception related to files larger than 2GB. But since I used Ranges no exception will throw.
             // Setting client.MaxResponseContentBufferSize to long.MaxValue will cause a exception! 2147483647 is the limit
             {
+                var statusCode = response.StatusCode;
                 var content = response.Content;
-                if (content == null) return new DownloadInfo { StatusCode = response.StatusCode };
+                if (content == null) return new DownloadInfo { StatusCode = statusCode };
                 DownloadInfo downloadInfo = new DownloadInfo
                 {
                     ContentLength = content.Headers.ContentLength,
                     ContentType = content.Headers.ContentType.MediaType,
                     StatusCode = response.StatusCode,
-                    ServerSideFileName = content.Headers.ContentDisposition?.FileName
+                    ServerSideFileName = content.Headers.ContentDisposition?.FileName,
+                    PartialContent = statusCode == HttpStatusCode.PartialContent
                 };
-                if (downloadInfo.ContentLength.HasValue && response.IsSuccessStatusCode)
-                {
-                    if (downloadInfo.ContentLength.Value <= int.MaxValue)
-                    {
-                        downloadInfo.CanDetectPartialContent = true;
-                        var statusCode = await GetStatusCodeAsync(uri).ConfigureAwait(false);
-                        downloadInfo.StatusCode = statusCode;
-                        downloadInfo.PartialContent = statusCode == HttpStatusCode.PartialContent;
-                    }
-                    else
-                        downloadInfo.CanDetectPartialContent = downloadInfo.PartialContent = false;
-                }
+                if (response.Content.Headers.ContentRange?.HasLength ?? false)
+                    downloadInfo.ContentLength = response.Content.Headers.ContentRange.Length;
+                //if (downloadInfo.ContentLength.HasValue && response.IsSuccessStatusCode)
+                //{
+                //    if (downloadInfo.ContentLength.Value <= int.MaxValue)
+                //    {
+                //        downloadInfo.CanDetectPartialContent = true;
+                //        //var statusCode = await GetStatusCodeAsync(uri).ConfigureAwait(false);
+                //        downloadInfo.StatusCode = statusCode;
+                //        downloadInfo.PartialContent = statusCode == HttpStatusCode.PartialContent;
+                //    }
+                //    else
+                //        downloadInfo.CanDetectPartialContent = downloadInfo.PartialContent = false;
+                //}
                 return downloadInfo;
             }
-        }
-        private static async Task<HttpStatusCode> GetStatusCodeAsync(Uri uri)
-        {
-            using (var client = new HttpClient { Timeout = TimeSpan.FromSeconds(40) })
-            using (var request = new HttpRequestMessage(HttpMethod.Head, uri))
-            using (var response = await client.SendAsync(request)) 
-                // since our file is lower than 2GB we can avoid ResponseHeadersRead part
-                // now we can actually get the partial content, if possible
-                return response.StatusCode;
         }
     }
 }
